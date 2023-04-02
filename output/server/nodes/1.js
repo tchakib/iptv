0d76fd4bde6b370956d670ce704e7dd8f1e16339
tchakib <49452389+tchakib@users.noreply.github.com>
2023-04-02 14:07:11 +0000

    Updates

--- output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const file = '_app/immutable/components/error.svelte-a7192f92.js';
-export const imports = ["_app/immutable/components/error.svelte-a7192f92.js","_app/immutable/chunks/index-44df08c7.js","_app/immutable/chunks/stores-500e7bc6.js","_app/immutable/chunks/singletons-b6a05648.js"];
-export const stylesheets = [];
-export const fonts = [];
--- output/server/nodes/3.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 3;
-export const component = async () => (await import('../entries/pages/channel/_page.svelte.js')).default;
-export const file = '_app/immutable/components/pages/channel/_page.svelte-2fc55ff6.js';
-export const imports = ["_app/immutable/components/pages/channel/_page.svelte-2fc55ff6.js","_app/immutable/chunks/index-44df08c7.js","_app/immutable/chunks/HTMLPreview-2ad8fa1f.js","_app/immutable/chunks/stores-500e7bc6.js","_app/immutable/chunks/singletons-b6a05648.js"];
-export const stylesheets = ["_app/immutable/assets/HTMLPreview-0d24e5da.css"];
-export const fonts = [];
